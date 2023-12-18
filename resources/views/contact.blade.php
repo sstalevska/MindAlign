e66d39ec6a5540814825ed017dc96d8a163c7bd1
Sara Stalevska <sara.stalevska@gmail.com>
2023-12-18 01:34:12 +0100

    Initial commit.

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<x-guest-layout>
+    <div class="max-w-3xl">
+        <div class="bg-white rounded-md shadow-md p-5">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla quo numquam nisi fugit doloribus ipsa eveniet, temporibus nam eos sit hic, adipisci neque iure repellendus inventore dolore eum recusandae enim?
+        </div>
+    </div>
+</x-guest-layout>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Public routes
+Route::view('/', 'welcome');
+Route::view('/contact', 'contact');
+
+// Auth routes
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
