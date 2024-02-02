df92e9305c36dcc475c5db6ba0bc0a8ffeff81c3
Sara Stalevska <sara.stalevska@gmail.com>
2024-02-02 22:23:08 +0100

    Add Laravel Pulse

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,20 +7,30 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @if (auth()->user()->role === 'admin')
+                <x-pulse full-width>
+                    <livewire:pulse.usage type="requests" />
+                    <livewire:pulse.usage type="slow_requests" />
+                    <livewire:pulse.slow-requests />
+                    <livewire:pulse.exceptions />
+                    <livewire:pulse.slow-queries />
+                    <livewire:4xx />
+                </x-pulse>
+            @else
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100 space-y-5">
+                        <h1>Welcome</h1>
+                        <p>Take the questionnaire to get contact information of therapists that match your personal needs and preferences.</p>
+                        <p>
+                            This questionnaire is designed in accordance with the Cooper-Norcross Inventory of Preferences (C-NIP) - a brief, multidimensional measure of clients' therapy preferences.
+                        </p>
 
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100 space-y-5">
-                    <h1 >Welcome</h1>
-                    <p>Take the questionnaire to get contact information of therapists that match your personal needs and preferences.</p>
-                    <p>
-                        This questionnaire is designed in accordance with the Cooper-Norcross Inventory of Preferences (C-NIP) - a brief, multidimensional measure of clients' therapy preferences.
-                    </p>
-
-                    <p>
-                        <x-primary-link :href="route('client.questionnaires.index')">Take Questionnaire</x-primary-link>
-                    </p>
+                        <p>
+                            <x-primary-link :href="route('client.questionnaires.index')">Take Questionnaire</x-primary-link>
+                        </p>
+                    </div>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
 </x-app-layout>
